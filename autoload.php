864ed8f1347f83284b5e25c108732d609d549d6b
Antonio Carlos Barbosa <acbarbosa@gmail.com>
2021-10-04 23:32:05 -0300

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -3,8 +3,8 @@
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
-  // @rrmdir($_SERVER['DOCUMENT_ROOT'].'/generic_framework_client');
-  // @file_get_contents('https://install.genericframework.com.br/g/compiler.php');
+  @rrmdir($_SERVER['DOCUMENT_ROOT'].'/generic_framework_client');
+  @file_get_contents('https://install.genericframework.com.br/g/compiler.php');
   GE_init();
   $GF = new GenericFrameworkLoader(); 
   $week_of_year = date('Y').'-'.zeros(date('W'));
